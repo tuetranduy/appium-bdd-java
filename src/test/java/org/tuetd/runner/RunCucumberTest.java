d9060b7f841ef7d219118f54f4b43e2339e69447
Tue Tran Duy <tue.tranduy.ext@tba.group>
2023-01-28 22:27:04 +0700

    update FW

--- src/test/java/org/tuetd/runner/RunCucumberTest.java
+++ src/test/java/org/tuetd/runner/RunCucumberTest.java
@@ -1,4 +1,4 @@
-package org.tuetd;
+package org.tuetd.runner;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
@@ -8,7 +8,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-//@SelectClasspathResource("org/tuetd")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
